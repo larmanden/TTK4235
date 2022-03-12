7beedaeef1fdd6a4aaf635c3410d68b22f01a94f
Lars Moan <lars.ostberg.moan@gmail.com>
2022-03-12 14:20:30 +0100

    doxygen documentation

--- source/elevator.h
+++ source/elevator.h
@@ -1,15 +1,32 @@
+/**
+ * @file elevator.h
+ * @author your name (you@domain.com)
+ * @brief The Elevator, it's states and corresponding functions for the Elevator.
+ * @version 0.1
+ * @date 2022-03-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #pragma once
 #include "driver/elevio.h"
 #include "queue.h"
 
-
+/**
+ * @brief All possible states that the elevator can be in.
+ * 
+ */
 typedef enum {
     IDLE = 0, 
     MOVING, EMERGENCY_STOP,
     DOOR_OPEN
 }STATE;
 
-
+/**
+ * @brief The elevators and it's corresponding member variables.
+ * 
+ */
 struct elevator{
     MotorDirection prev_motor_dir;
     MotorDirection current_motor_dir;
@@ -20,15 +37,72 @@ struct elevator{
 };
 
 typedef struct elevator elevator;
-
+/**
+ * @brief Updates the elevators current direction to @p newdir as well as updating previous direction.
+ * 
+ * @param [in,out] el The elevator.
+ * @param [in] newdir New direction for the elevator.
+ */
 void elev_update_dir(elevator* el, MotorDirection newdir);
+
+/**
+ * @brief Checks wether the queue has any orders.
+ * 
+ * @param [in,out] el The elevator.
+ * @return 1 if there is a button pressed, 0 if not.
+ */
 int elev_has_order(elevator* el);
+
+/**
+ * @brief Checks if there is an order above the elevator's current position.
+ * 
+ * @param [in,out] el The elevator.
+ * @return 1 if there is an order above, 0 if not.
+ */
 int elev_order_above(elevator* el);
+
+/**
+ * @brief Checks if there is an order below the elevator's current position.
+ * 
+ * @param [in,out] el The elevator.
+ * @return 1 if there is an order below, 0 if not. 
+ */
 int elev_order_below(elevator* el);
+
+/**
+ * @brief Removes all orders to the elevator's current floor.
+ * 
+ * @param [in, out] el The elevator.
+ */
 void elev_remove_last_order(elevator* el);
+
+/**
+ * @brief Updates the elevator's current floor if it is in a defined floor.
+ * 
+ * @param [in, out] el The elevator.
+ */
 void elev_update_current_floor(elevator* el);
+
+/**
+ * @brief Finds the furthest button pressed in the opposite direction of the elevator's current direction.
+ * 
+ * @param [in,out] el The elevator.
+ * @return The floorindex to the furthest entry in opposite direction.
+ */
 int elev_look_ahead(elevator* el);
 
+/**
+ * @brief Function for deciding wether to go upwards og downwards after an emergency between two floors.
+ * 
+ * @param [in,out] el The elevator.
+ * @return MotorDirection, direction the elevator should go.
+ */
 MotorDirection elev_move_after_emergency(elevator* el);
+
+/**
+ * @brief Turns on a buttonlight if the button is pressed. 
+ * 
+ * @param [in] queue The elevators queue system.
+ */
 void elev_btnlights_update(int queue[N_BUTTONS][N_FLOORS]);
 
--- source/queue.h
+++ source/queue.h
@@ -1,10 +1,37 @@
+/**
+ * @file queue.h
+ * @author your name (you@domain.com)
+ * @brief Functions for the elevators queue system. The queue is defined as a 3x4 matrix.
+ * @version 0.1
+ * @date 2022-03-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #pragma once
 #include "driver/elevio.h"
 #include <stdio.h>
 #include "stdlib.h"
 #include "elevator.h"
 
+/**
+ * @brief Clears the queue by setting all entries in the matrix to 0. Entries corresponding to buttons that doesn't exist is set to -1.
+ * 
+ * @param [in,out] queue Elevator's queue.
+ */
 void queue_clear(int queue[N_BUTTONS][N_FLOORS]);
+
+/**
+ * @brief Prints the queue to terminal.
+ * 
+ * @param [in] queue 
+ */
 void queue_print(int queue[N_BUTTONS][N_FLOORS]);
+
+/**
+ * @brief Updates entries in the queue if the given button is pressed on the panel.
+ * 
+ * @param [in, out] queue 
+ */
 void queue_update(int queue[N_BUTTONS][N_FLOORS]);
 
